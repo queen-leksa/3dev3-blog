0ff4a9d2c6258d1a21f845e217eb883c26a71a7d
Leksa <surnacheva@ithub.ru>
2022-09-16 17:14:04 +0300

    add posts card

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-
+import posts from "./data/posts.json";
 export default () => {
     return <>
         <Header/>
-        <Main/>
+        <Main data={posts}/>
         <Footer/>
     </>
 }
